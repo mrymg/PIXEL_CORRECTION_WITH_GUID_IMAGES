3b6029c3be32c59e2ebe2a151f265fc7763e1029
YMG <ymgozutok@live.com>
2020-01-14 02:03:58 +0300

    Update GUID.py

--- GUID.py
+++ GUID.py
@@ -5,7 +5,7 @@ import os
 def pid():
     return uuid.uuid4()
 
-path = "/Users/ymg/Desktop/BBM/BBM 419/final_197/"
+path = "C:\\Users\\ymgoz\\Desktop\\assigned-to-yunus2\\WITH_PIXEL_CORRECTION\\final_197_son\\"
 names = list()
 for file in os.listdir(path):
     names.append(file)
@@ -14,5 +14,5 @@ names.sort()
 
 for i in range(0, len(names), 2):
     idm = pid()
-    os.rename(path + names[i], '/Users/ymg/Desktop/BBM/BBM 419/guided3/' + str(idm) + '.png')
-    os.rename(path + names[i+1], '/Users/ymg/Desktop/BBM/BBM 419/guided3/' + str(idm) + '_L.png')
+    os.rename(path + names[i], 'C:\\Users\\ymgoz\\Desktop\\assigned-to-yunus2\\WITH_PIXEL_CORRECTION\\GUIDED_3\\' + str(idm) + '.png')
+    os.rename(path + names[i+1], 'C:\\Users\\ymgoz\\Desktop\\assigned-to-yunus2\\WITH_PIXEL_CORRECTION\\GUIDED_3\\' + str(idm) + '_L.png')
