5365031b855829a971c0f34f5c5a385c1a819667
YMG <ymgozutok@live.com>
2020-01-24 01:02:49 +0300

    Update GUID.py

--- GUID.py
+++ GUID.py
@@ -5,14 +5,16 @@ import os
 def pid():
     return uuid.uuid4()
 
-path = "C:\\Users\\ymgoz\\Desktop\\assigned-to-yunus2\\WITH_PIXEL_CORRECTION\\final_197_son\\"
+SOURCE = "SOURCE PATH FOR TO GUID IMAGES"
+DST = "DESTINATION PATH TO NEW IMAGES"
+
 names = list()
-for file in os.listdir(path):
+for file in os.listdir(SOURCE):
     names.append(file)
 
 names.sort()
 
 for i in range(0, len(names), 2):
     idm = pid()
-    os.rename(path + names[i], 'C:\\Users\\ymgoz\\Desktop\\assigned-to-yunus2\\WITH_PIXEL_CORRECTION\\GUIDED_3\\' + str(idm) + '.png')
-    os.rename(path + names[i+1], 'C:\\Users\\ymgoz\\Desktop\\assigned-to-yunus2\\WITH_PIXEL_CORRECTION\\GUIDED_3\\' + str(idm) + '_L.png')
+    os.rename(SOURCE + names[i], DST + str(idm) + '.png')
+    os.rename(SOURCE + names[i+1], DST + str(idm) + '_L.png')
